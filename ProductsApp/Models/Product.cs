4ffa6b7aec3bead035983351c5dd168511ee4012
VijaiPatel <48962282+VijaiPatel@users.noreply.github.com>
2023-06-30 15:49:18 +0100

    Added Data Annotations

--- ProductsApp/Models/Product.cs
+++ ProductsApp/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -10,8 +11,14 @@ namespace ProductApp.Models
     {
         [Key]
         public int Id { get; set; }
+        [DisplayName("product name")]
+        [StringLength(10)]
         public string ProductName { get; set; }
+        [Required(ErrorMessage = "price required")]
+        [Range(0, 100, ErrorMessage = "price must be between 0 and 100")]
         public decimal Price { get; set; }
+        [DisplayName("description")]
+        [StringLength(50)]
         public string Description { get; set; }
     }
 }
